a4376e1f5c7e881af38512158562f355ca7de3da
Marina <marina.zilkina@gmail.com>
2023-01-22 16:42:19 +0200

    random colors; matching colors text w buttons;

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('working')
+const cols = document.querySelectorAll('.col')
+
+function generateRandomColor() {
+    //RGB палирта состоит из этих символов
+    const hexCodes = '0123456789ABCDEF'
+    // будущий цвет попадет сюда
+    let color = ''
+    for (let i = 0; i<6; i++){
+        color +=hexCodes[Math.floor(Math.random()* hexCodes.length)]
+    }
+    return '#' + color
+}
+
+function setRandomColors() {
+    cols.forEach(col => {
+        const text = col.querySelector('h2')
+        const button = col.querySelector('button')
+        const color = chroma.random() // вместо generateRandomColor()
+        text.textContent = color
+        col.style.background = color
+
+        setTextColor(text, color)
+        setTextColor(button, color)
+    });
+}
+
+function setTextColor (text, color) {
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+
+}
+setRandomColors()
